41edc8511ff85ee6a0c9e6f512edea2302dc4f42
kozak <kozak@192.168.1.20>
2025-01-05 12:33:45 +0300

    Supressed the global prisma type error

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,7 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
+// @ts-ignore
 export const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+    // @ts-ignore
+    global.prisma = prisma;
+};
 
 export * from '@prisma/client';
